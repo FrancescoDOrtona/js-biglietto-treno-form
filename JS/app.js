0549406599ed16c16ac979ed17e4f28882c99e6d
FrancescoDOrtona <francescod.97@hotmail.it>
2023-10-05 13:51:36 +0200

    Add: HTML, CSS, JS file with link & script

---
+++ JS/app.js
@@ -0,0 +1,10 @@
+// # Form biglietto treno
+
+// - Inserisco gli input in html con le relative class, id e value
+// - Dichiaro le variabili globali
+//     - prendo gli elementi dal DOM
+// - Recupero dall'input il valore dei value
+//     - prendo il valore dei km  e calcolo il prezzo del biglietto per 0.21€ al km
+//     - prendo il valore degli sconti e calcolo gli sconti se minorenni o >65 anni
+// - Calcolo il prezzo finale del biglietto
+// - Stampo il prezzo nel DOM
