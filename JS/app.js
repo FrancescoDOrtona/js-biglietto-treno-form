87c3e457c8d70b0a87662645972b19f1936e6d9f
FrancescoDOrtona <francescod.97@hotmail.it>
2023-10-05 14:06:09 +0200

    Add: Variables from DOM Elements

--- JS/app.js
+++ JS/app.js
@@ -3,6 +3,18 @@
 // - Inserisco gli input in html con le relative class, id e value
 // - Dichiaro le variabili globali
 //     - prendo gli elementi dal DOM
+const distanceDOMElement = document.getElementById('distance')
+console.log(distanceDOMElement)
+
+const discountDOMElement = document.getElementById('discount')
+console.log(discountDOMElement)
+
+const btnDOMElement = document.getElementById('btn-price')
+console.log(btnDOMElement)
+
+const priceDOMElement = document.getElementById('price')
+console.log(priceDOMElement)
+
 // - Recupero dall'input il valore dei value
 //     - prendo il valore dei km  e calcolo il prezzo del biglietto per 0.21€ al km
 //     - prendo il valore degli sconti e calcolo gli sconti se minorenni o >65 anni
